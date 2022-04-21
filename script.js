75ba7989046df3ae3ade7e8d8750df16156c8235
osam7a <osamaalhinawi2008@gmail.com>
2022-04-22 03:54:53 +0400

    added math functions (abs cos sqrt sin round PI log)

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function reset() {
 }
 
 // Evaluate equation
-function evaluteEquation() {
+function evaluateEquation() {
     var equation = document.getElementById('output');
     if (equation.innerHTML === "Result will be shown here...") {
         equation.innerHTML = "Invalid equation!";
@@ -66,6 +66,48 @@ function addNumbers(div) {
     }
 }
 
+function doSqrt() {
+    const output = document.getElementById("output");
+    if (output.innerHTML === "Result will be shown here...") { return }
+    evaluateEquation();
+    output.innerHTML = Math.sqrt(output.innerHTML);
+}
+
+function doSin() {
+    const output = document.getElementById("output");
+    if (output.innerHTML === "Result will be shown here...") { return }
+    evaluateEquation();
+    output.innerHTML = Math.sin(output.innerHTML);
+}
+
+function doRound() {
+    const output = document.getElementById("output");
+    if (output.innerHTML === "Result will be shown here...") { return }
+    evaluateEquation();
+    output.innerHTML = Math.round(output.innerHTML);
+}
+
+function doCos() {
+    const output = document.getElementById("output");
+    if (output.innerHTML === "Result will be shown here...") { return }
+    evaluateEquation();
+    output.innerHTML = Math.cos(output.innerHTML);
+}
+
+function doAbs() {
+    const output = document.getElementById("output");
+    if (output.innerHTML === "Result will be shown here...") { return }
+    evaluateEquation();
+    output.innerHTML = Math.abs(output.innerHTML);
+}
+
+function doLog() {
+    const output = document.getElementById("output");
+    if (output.innerHTML === "Result will be shown here...") { return }
+    evaluateEquation();
+    output.innerHTML = Math.log(output.innerHTML);
+}
+
 // Calculator initialization
 function initializeCalculator(div) {
     // Create elements
@@ -76,6 +118,13 @@ function initializeCalculator(div) {
     const minus = document.createElement("button");
     const multiply = document.createElement("button");
     const plus = document.createElement("button");
+    const sqrt_ = document.createElement("button");
+    const round_ = document.createElement("button");
+    const sin_ = document.createElement("button");
+    const cos_ = document.createElement("button");
+    const abs_ = document.createElement("button");
+    const log_ = document.createElement("button");
+    const PI_ = document.createElement("button");
 
     // Set text to each button
     evaluate.appendChild(document.createTextNode("="));
@@ -84,8 +133,15 @@ function initializeCalculator(div) {
     zero.appendChild(document.createTextNode("0"));
     multiply.appendChild(document.createTextNode("*"));
     minus.appendChild(document.createTextNode("-"));
-    plus.appendChild(document.createTextNode("+"))
-
+    plus.appendChild(document.createTextNode("+"));
+    sqrt_.appendChild(document.createTextNode('√'));
+    PI_.appendChild(document.createTextNode("π"));
+    abs_.appendChild(document.createTextNode("|x|"));
+    log_.appendChild(document.createTextNode("log"));
+    cos_.appendChild(document.createTextNode("cos"));
+    sin_.appendChild(document.createTextNode("sin"));
+    round_.appendChild(document.createTextNode("round"));
+    
     // Set attributes
     reset.setAttribute("onclick", `reset()`);
     divide.setAttribute("onclick", "appendText(\"/\")");
@@ -93,7 +149,14 @@ function initializeCalculator(div) {
     plus.setAttribute("onclick", "appendText(\"+\")");
     minus.setAttribute("onclick", "appendText(\"-\")");
     zero.setAttribute("onclick", "appendText(\"0\")");
-    evaluate.setAttribute("onclick", "evaluteEquation()");
+    PI_.setAttribute("onclick", `appendText(\"${Math.PI}\")`);
+    sqrt_.setAttribute("onclick", "doSqrt()");
+    abs_.setAttribute("onclick", "doAbs()");
+    round_.setAttribute("onclick", "doRound()");
+    sin_.setAttribute("onclick", "doSin()");
+    cos_.setAttribute("onclick", "doCos()");
+    log_.setAttribute("onclick", "doLog()");
+    evaluate.setAttribute("onclick", "evaluateEquation()");
     
 
     // Set styles
@@ -103,8 +166,16 @@ function initializeCalculator(div) {
     plus.setAttribute("class", "numpad");
     minus.setAttribute("class", "numpad");
     zero.setAttribute("class", "numpad");
+    PI_.setAttribute("class", "numpad");
+    sqrt_.setAttribute("class", "numpad");
+    cos_.setAttribute("class", "numpad");
+    sin_.setAttribute("class", "numpad");
+    round_.setAttribute("class", "numpad");
+    abs_.setAttribute("class", "numpad");
+    log_.setAttribute("class", "numpad")
     reset.style.width = '50px';
     reset.style.height = '50px';
+    round_.style.fontSize = '45px';
 
     // Append to main div
     addNumbers(div);
@@ -112,9 +183,22 @@ function initializeCalculator(div) {
     div.appendChild(zero);
     div.appendChild(evaluate);
     div.appendChild(document.createElement("br"));
+    div.appendChild(document.createElement("br"));
     div.appendChild(plus);
     div.appendChild(minus);
     div.appendChild(multiply);
+    div.appendChild(document.createElement("br"));
+    div.appendChild(document.createElement("br"));
+    div.appendChild(sqrt_);
+    div.appendChild(PI_);
+    div.appendChild(abs_);
+    div.appendChild(document.createElement("br"));
+    div.appendChild(log_);
+    div.appendChild(round_);
+    div.appendChild(cos_);
+    div.appendChild(document.createElement("br"));
+    div.appendChild(sin_);
+    div.appendChild(document.createElement("br"));
     div.appendChild(document.createElement("br"));
     div.appendChild(reset);
 }
